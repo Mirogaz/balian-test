9a0a509511aeef9f6bc386a362d947a01b525811
dezember <ver.ver.sus.sus@mail.ru>
2023-12-12 20:50:17 +0300

    fix

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 import styles from './header.module.scss';
 import Logo from '@img/logo.svg';
 import { useTelegram } from '@hooks/useTelegram';
+import { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronDown} from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
 
-  const { user } = useTelegram();
+  const { tg, user } = useTelegram();
+
+  useEffect(() => {
+    tg.ready();
+  }, [tg])
+
+
+  console.log(user)
 
   return (
     <header className={styles.header}>
       <img src={Logo} alt="Logo" />
-      <h1 class={styles.header__text}>BALIAN</h1>
+      <h1 className={styles.header__text}>BALIAN</h1>
       <div className={styles.header__language}>
         <FontAwesomeIcon icon={faChevronDown} />
       </div>
